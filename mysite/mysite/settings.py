03b2b525532466d4f9b702afe67d64c06f6557ea
Nikita Petrov <Nikita2871@yandex.ru>
2023-06-27 14:57:20 +0300

    configure for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,46 +9,47 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import logging.config
+from os import getenv
 from pathlib import Path
-
 from django.urls import reverse_lazy
 
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-import sentry_sdk
-sentry_sdk.init(
-    dsn="https://d9109192c2ef401e83db258ceb681390@o4505346499215360.ingest.sentry.io/4505369821184000",
-    traces_sample_rate=1.0
-)
+DATABASE_DIR = BASE_DIR / 'database'
+DATABASE_DIR.mkdir(exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z2trcc6=**ds@5p$b^_eck_@hcs+t#c^kbi1g8%3kl_5ck%87v'
+SECRET_KEY = getenv(
+    'DJANGO_SECRET_KEY',
+    'django-insecure-z2trcc6=**ds@5p$b^_eck_@hcs+t#c^kbi1g8%3kl_5ck%87v',
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = getenv('DJANGO_DEBUG', '0') == '1'
 
 ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '127.0.0.1',
-]
+                    '0.0.0.0',
+                    '127.0.0.1',
+                ] + getenv('DJANGO_ALLOWED_HOSTS', "").split(',')
+
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
 if DEBUG:
     import socket
+
     hostname, __, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS.append('10.0.2.2')
     INTERNAL_IPS.extend(
-        #192.168.1.1
-        [ip[ ip.find('.')] + '.1' for ip in ips]
+        # 192.168.1.1
+        [ip[ip.find('.')] + '.1' for ip in ips]
     )
 
 # Application definition
@@ -119,7 +120,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': DATABASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -206,36 +207,28 @@ SPECTACULAR_SETTINGS = {
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
-LOGFILE_NAME = BASE_DIR / 'log.txt'
-LOGFILE_SIZE = 1 * 1024 * 1024
-LOGFILE_COUNT = 3
+LOGLEVEL = getenv('DJANGO_LOGLEVEL', "info").upper()
 
-LOGGING = {
+logging.config.dictConfig({
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'verbose': {
-            'format': '%(asctime)s %(levelname)s %(name)s: %(message)s',
-        }
+        'console': {
+            'format': '%(asctime)s %(levelname)s [%(name)s:%(lineno)s] %(module)s %(message)s',
+        },
     },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
-            'formatter': 'verbose',
-        },
-        'logfile': {
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': LOGFILE_NAME,
-            'maxBytes': LOGFILE_SIZE,
-            'backupCount': LOGFILE_COUNT,
-            'formatter': 'verbose',
+            'formatter': 'console',
         },
     },
-    'root': {
-        'handlers': [
-            'console',
-            'logfile'
-        ],
-        'level': 'INFO',
-    },
-}
+    'loggers': {
+        '': {
+            'level': LOGLEVEL,
+            'handlers': [
+                'console',
+            ],
+        },
+},
+})
